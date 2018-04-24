14f8a0d9f3c6b1715fdeb1c199a3a7c570fc4f07
luantb <devtrluan@gmail.com>
2018-04-24 20:21:55 +0700

    luan test

---
+++ ProjectSem2/src/Utils/InternalFrameFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bkap.itrd.qlts.utils;
+
+import bkap.itrd.qlts.internalframe.DangNhap;
+import bkap.itrd.qlts.internalframe.QuanLyNhanVien;
+import bkap.itrd.qlts.internalframe.QuanLyTaiSan;
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author MinhVuFC
+ */
+public class InternalFrameFactory {
+
+    public JInternalFrame getInternalFrame(int id) {
+        switch (id) {
+            case Constant.QUAN_LY_TAI_SAN:
+                return new QuanLyTaiSan();
+            case Constant.QUAN_LY_NHAN_VIEN:
+                return new QuanLyNhanVien();
+            default:
+                return DangNhap.getInstance();
+        }
+    }
+}
